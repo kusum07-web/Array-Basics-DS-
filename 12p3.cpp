9e8fda96b0e69d2c67c363726e69bcaeb2ed103d
kusum07-web <kusumehra07@gmail.com>
2026-05-25 15:42:37 +0530

    final changes

--- 12p3.cpp
+++ 12p3.cpp
@@ -1,1 +1,16 @@
 //WAF to print all the unique numbers in an array
+#include<iostream>
+using namespace std;
+int uniqueNum(int arr[],int sz){
+    int unique=0;
+    for(int i=0;i<sz;i++){
+        unique=unique^arr[i];
+    }
+    return unique;
+}
+int main(){
+    int arr[]={2,4,4,1,1,3,3,6};
+    int sz=8;
+    int unique=uniqueNum(arr,sz);
+    cout<<"Unique Number is: "<<unique;
+}
--- 13p4.cpp
+++ 13p4.cpp
@@ -1,1 +1,22 @@
 //WAF to print intersection of r arrays
+#include<iostream>
+using namespace std;
+int Intersection(int arr1[],int sz1,int arr2[],int sz2){
+    cout<<"Intersected elements are:";
+    for(int i=0;i<sz1;i++){
+        for(int j=0;j<sz2;j++){
+            if(arr1[i]==arr2[j]){
+                
+                cout<<arr1[i]<<" ";
+            }
+        }
+    }
+}
+int main(){
+    int arr1[]={2,4,6,8};
+    int arr2[]={1,2,4,3};
+    int sz1=4;
+    int sz2=4;
+    Intersection(arr1,sz1,arr2,sz2);
+    return 0;
+}
